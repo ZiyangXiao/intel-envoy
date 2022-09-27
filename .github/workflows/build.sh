c8649ad9f38838d47a84a84865ce2f93a5338c8a
Xiao, Ziyang <ziyang.xiao@intel.com>
2022-09-27 14:53:09 +0800

    Add build and push step CI
    Signed-off-by: Xiao, Ziyang <ziyang.xiao@intel.com>

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -8,51 +8,32 @@
 #first build should give permission to docker volumes
 #sudo chmod -R 777 /var/lib/docker/volumes 
 
-branch_name=${branch_name:-"release-1.15-intel"}
-cd ..
-if [ ! -d "istio-proxy" ];
-then
-    git clone -b ${branch_name} https://github.com/intel/istio-proxy.git
-else
-    pushd istio-proxy
-    git clean -fdx
-    git fetch origin
-    git reset --hard origin/${branch_name} 
-    popd
-fi
+UPDATE_BRANCH=${UPDATE_BRANCH:-"release-1.15-intel"}
 # To maintain build repo is latest
-# should modified when pr to intel/envoy
-cp -rf intel-envoy/ istio-proxy/ 
+cd ..
+rm -rf istio-proxy
+git clone -b ${UPDATE_BRANCH} https://github.com/intel/istio-proxy.git
+cp -rf envoy/ istio-proxy/ 
 cd istio-proxy
-if [ ! -d "istio" ];
-then
-    git clone -b ${branch_name} https://github.com/intel/istio.git
-else
-    pushd istio
-    git clean -fdx
-    git fetch origin
-    git reset --hard origin/${branch_name} 
-    popd
-fi
+git clone -b ${UPDATE_BRANCH} https://github.com/intel/istio.git
 
 # Replace upstream envoy with local envoy in build file
 # In envoy repo we still use sed method because we need to catch PR. Only use update_envoy.sh cannot get pr patch.
 cp -f WORKSPACE WORKSPACE.bazel
 sed  -i '/http_archive(/{:a;N;/)/!ba;s/.*name = "envoy".*/local_repository(\
     name = "envoy",\
-    path = "intel-envoy",\
+    path = "envoy",\
 )/g}' WORKSPACE.bazel
 
 # build envoy binary in container with sgx
-IMG="registry.fi.intel.com/xintongc/build-tools-proxy:master-latest-sgx" BUILD_WITH_CONTAINER=1 make build_envoy 
-IMG="registry.fi.intel.com/xintongc/build-tools-proxy:master-latest-sgx" BUILD_WITH_CONTAINER=1 make exportcache
-# build istio.istio
-cd istio
-# export env
-export TAG=${TAG:-"pre-build"}
-make build
+BUILD_WITH_CONTAINER=1 make build_envoy 
+BUILD_WITH_CONTAINER=1 make exportcache
+# sgx build container would cause build proxyv2 image failed in release-1.15
+unset IMG
+# build istio and export env
+TAG=${TAG:-"pre-build"}
+(cd istio;make build)
 # replace upstream envoy with local envoy in build proxyv2 image
-cd  ..
 cp -rf out/linux_amd64/envoy istio/out/linux_amd64/release/envoy
 # build proxyv2 image
 cd istio
